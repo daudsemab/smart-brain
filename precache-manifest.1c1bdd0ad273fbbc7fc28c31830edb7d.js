c791172e67712fa8d44a74f49a981bd708f1996d
Daud Semab <daudsemab.dxb@gmail.com>
2020-06-28 15:04:15 +0500

    Updates

---
+++ precache-manifest.1c1bdd0ad273fbbc7fc28c31830edb7d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816368cc9a422fe8d263c6a41ff94aee",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "3a6c4ad973db170a481f",
+    "url": "/smart-brain/static/css/main.f70c275b.chunk.css"
+  },
+  {
+    "revision": "84e8506502d99a23d142",
+    "url": "/smart-brain/static/js/2.559c25d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.559c25d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a6c4ad973db170a481f",
+    "url": "/smart-brain/static/js/main.631dbe0a.chunk.js"
+  },
+  {
+    "revision": "0f18f76651f6f4d6c8f2",
+    "url": "/smart-brain/static/js/runtime-main.12188e31.js"
+  },
+  {
+    "revision": "9b94499ccea3bd82b24cb210733c4b5e",
+    "url": "/smart-brain/static/media/Pacifico-Regular.9b94499c.ttf"
+  }
+]);
